92eb7775e1683cb79608676d3ac133aedd7f9de8
MrS2ud <54178651+MrS2ud@users.noreply.github.com>
2019-08-16 17:48:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -158,5 +158,24 @@ const args = message.content.slice(prefix.length).trim().split(/ +/g);
 }
 })
 
+client.on("guildMemberAdd", member => {
+  let welcome = member.guild.channels.find("welcome","اسم روم الترحيب");
+  if(!welcome) return;
+  if(welcome) {
+      let embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setThumbnail(member.user.avatarURL)  
+      .setAuthor(member.user.username , member.user.avatarURL)
+      .addField("**Welcome To**", `[${member.guild.name}]`, true)
+      .addField(`**Number**`, `[${member.guild.memberCount}]`, true)
+      .addField("**Name**", `[${member.user.username}#${member.user.discriminator}]`,true)
+      .addField("**ID**", `[${member.user.id}]`, true)
+      .addField('**Created AT**',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')}**n** `${moment(member.user.createdAt).fromNow()}``,true)            
+      .addField("**Joined At**",`${moment(member.user.joinedAt).format('D/M/YYYY h:mm a')}**n** `${moment(member.user.joinedAt).fromNow()}``,true)    
+      welcome.send(embed)
+
+  }
+  })
+
 
 client.login(process.env.BOT_TOKEN);
