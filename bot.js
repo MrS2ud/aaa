2c3ab7239c8b7669e1bbbc8e21dd25e624c71551
MrS2ud <54178651+MrS2ud@users.noreply.github.com>
2019-08-16 17:39:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,7 +139,7 @@ client.on("message", message => {
 
 
 client.on('message', message => {let prefix = "!";
-if(message.content.startsWith(prefix + "news")) {
+if(message.content.startsWith(prefix + "sug")) {
       message.delete()
 
 const args = message.content.slice(prefix.length).trim().split(/ +/g);
