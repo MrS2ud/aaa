a20b87ad57063825fac6725705e2cc77650a56a6
MrS2ud <54178651+MrS2ud@users.noreply.github.com>
2019-09-04 20:51:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,44 +1,16 @@
-const Discord = require("discord.js");
-
+const Discord = require('discord.js');
 const client = new Discord.Client();
-
-var prefix = "!";
-
+ 
 client.on('ready', () => {
-
-   console.log(`----------------`);
-
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-
-        console.log(`----------------`);
-
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-
-    console.log(`----------------`);
-
   console.log(`Logged in as ${client.user.tag}!`);
-
-client.user.setGame(`Security @ Legend eSports`,"http://twitch.tv/S-F")
-
-client.user.setStatus("dnd")
-
 });
-
-
-client.on('message', message => {
-   if (message.content === "!id") {
-   let embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setThumbnail(message.author.avatarURL)
-  .addField("Name:",`${message.author.username}`, true)
-  .addField('Discrim:',"#" +  message.author.discriminator, true)
-  .addField("ID:", message.author.id, true)
-  .addField("Create At:", message.author.createdAt, true)
-     
-     
-  message.channel.sendEmbed(embed);
-    }
-});
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 client.login(process.env.BOT_TOKEN);
